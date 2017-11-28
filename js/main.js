9518fee55a8435999df9785d8f9895ce3f4fe31b
Dan Losapio <danlosapio@gmail.com>
2017-11-28 14:25:40 -0500

    add responsive layout

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-const about = document.querySelector("#nav")
+// const about = document.querySelector("#nav")
 
-about.addEventListener('click', onLinkClick, false);
+// about.addEventListener('click', onLinkClick, false);
 
-function onLinkClick() {
-  document.getElementsByTagName('h2')[3].scrollIntoView();
-  // will scroll to 4th h2 element
+// function onLinkClick() {
+//   document.getElementsByTagName('h2')[3].scrollIntoView();
+//   // will scroll to 4th h2 element
+// }
+
+function myFunction() {
+    var x = document.getElementById("main-nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
 }
